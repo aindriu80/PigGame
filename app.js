f6705535485ce915805a768bac3eb41d8529eed8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-06-27 10:06:03 +0100

    DOM manipulation to change value on webpage

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 - Aindriu 26th June 2017
 */
+var scores, roundScore, activePlayer, dice;
+
+scores = [0.0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>';
